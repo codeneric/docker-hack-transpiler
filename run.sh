ee9d471cd42805d66ed77fc91b1f68f601171225
kingkrausel <denis@codeneric.com>
2017-11-11 11:04:47 +0100

    run the hack server

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-EXPOSED_PORT=8082
-NAME="hack-transpiler"
-docker run \
-    --name $NAME \
-    -v $(pwd)/bin:/hack-transpiler \
-    -d \
-    ubuntu:16.04 || docker start $NAME
-
-echo "Running on http://localhost:${EXPOSED_PORT}/wp-admin"
+docker run -t -d -v $(pwd):$(pwd) \
+ -e USERID=$UID \
+ --name hack \
+ registry.codeneric.com:5000/hack-transpiler
--- start-hack.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -t -d -v $(pwd):$(pwd) \
- -e USERID=$UID \
- --name hack \
- registry.codeneric.com:5000/hack-transpiler
